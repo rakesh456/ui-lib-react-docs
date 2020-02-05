b25bd7937a831ba85ae42a02e0e5d5d5bb0c1550
neeta1995 <neetasawant.ns@gmail.com>
2020-02-05 15:27:03 +0530

    table

--- src/stories/datePicker.stories.js
+++ src/stories/datePicker.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 //import { action } from '@storybook/addon-actions';
 import { datePicker } from '@storybook/react/demo';
+import './datePicker.css';
 
 export default {
   title: 'Date Picker',
@@ -13,6 +14,19 @@ export const Parameters = () => (
       <th>Sr. No.</th>
       <th>Names</th>
       <th>Purpose</th>
+      <th>Possible Values</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>Display Format</td>
+      <td>Format in which the date input should be accepted.  This will depend upon the setting of “Display Unit”</td>
+      <td><p>MM/DD/YYYY, ,DD/MM/YYYY, M (&Y)Q (&Y) and Y <br/><br/> Default: DD/MM/YYYY</p></td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Picker Icon Alignment</td>
+      <td>The calendar icon’s display location.</td>
+      <td><p>Left, Right <br/><br/> Default: Right</p></td>
     </tr>
   </table>
 );
